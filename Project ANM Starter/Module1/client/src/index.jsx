c7e682f3b6844fd1e0a91b7037a6d381901beb64
AmalAbed <amalaljarousha93@gmail.com>
2019-11-12 22:30:06 +0200

    get data from db

--- Project ANM Starter/Module1/client/src/index.jsx
+++ Project ANM Starter/Module1/client/src/index.jsx
@@ -4,59 +4,123 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state={
-           title:"Hello From the other side",
-           img:"",
-           desc:"",
-           img_author:"",
-           counter:868,
-           author_name:"Amal Al Jarousha",
-           date: new Date().toLocaleString(),
-        }
+        this.state = {
+            contents:[]
+        //    title:" ",
+        //    image:"",
+        //    description:"",
+        //    ImgAuther:"",
+        //    shares:0,
+        //    Auther:"",
+        //    date: new Date().toLocaleString(),
     }
-   
-    // getInformation(){
-    //     $.ajax({
-    //         url: "/repos",
-    //         method: "GET",
-    //         data: { username: term },
-    //         datatype: "json"
-    //         // success: function (a){console.log("Done")},
-    //         // error: function (){console.log("Fail")}
-    //       });
-    //    }
+      //  this.updateState=this.updateState.bind(this);
+    this.reriveData();
+    }
 
-    render(){
-     return(
-            <div id="content" class="col-xl-6">
-                    <h1 class="title">{this.state.title}</h1>
-                    <div class="social-shares">
-                        <div class="share-count" id="counter_shares">{this.state.counter}<span> Shares</span></div>
-                        <div class="share-widget share-article">
-                            <div class="social">
-                                <a class="facebook" href="#">
-                                    <img id="id_social" src="http://www.boldmethod.com/images/social/share-facebook-long-v2.png"/>
-                                </a>
-                            </div>
-                            <div class="social">
-                                <a href="#" class="twitter">
-                                <img id="id_social" src="https://images.squarespace-cdn.com/content/v1/563e2841e4b09a6ae020bd67/1530018807349-YP4LPA0C7INKN2XL0IM2/ke17ZwdGBToddI8pDm48kMJPBCPJsiaclxZdF5CWJnlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIMrHLJAYcctYfGny_wGpS1LuO2Ox8Cjc1KavDC4kMrXc/button+share+on+twitter.png"/>
-                                </a>
-                            </div> 
-                        </div>									
-                    </div>  
-                   <img id="img" src="https://cdn.iflscience.com/images/9402f0b2-ea1d-53eb-a72b-38ab70c67f6a/extra_large-1573087146-cover-image.jpg" alt="New Study Challenges One Of Darwin's Major Theories Surrounding The Beginning Of Life On Earth"></img>
-                      <div class="author" >
-                          <a href="https://www.iflscience.com/team/madison-dapcevich/" class="image">
-                            <img id="author_img"src="https://cdn.iflscience.com/images/2c71dbf8-f2ea-5a37-bd71-073bc362a79b/author_large-author.jpg"/> </a>
-								<h5 class="name">{this.state.author_name}</h5>
-								<span class="date">{this.state.date}</span>
-                               </div>      	
-								<div class="article-content">
-                                      <p>{this.state.desc}</p>
-                                </div>
-                          </div>        
-                        ) 
-                      }
-                    }
+    // IncrementItem = () => {
+    //     this.setState({ clicks: this.state.clicks + 1 });
+    //   }
+    //   ToggleClick = () => {
+    //     this.setState({ show: !this.state.show });
+    //   }
+    updateState(data){
+        this.setState({
+            contents:data
+        })               
+    }
+    // updateState([title1,image1,description1,ImgAuther1,shares1,Auther1,date1]){
+    //     this.setState({
+    //         title:title1,
+    //         image:image1,
+    //         description:description1,
+    //         ImgAuther:ImgAuther1,
+    //         shares:shares1,
+    //         Auther:Auther1,
+    //         date:date1
+    //    })
+    // }
+
+       increamentCounter(id){
+        $.ajax({
+            type: "POST",
+            url: "/shares",
+            data:{
+                user_id: id
+            }, 
+            datatype: "json",
+            success:function(){
+                console.log("success");
+            },
+            error: function(request, status, error) {
+                  console.log("error post");
+                }
+          });
+       }
+
+       
+      reriveData() {
+        console.log("get datat from client");
+        var that = this;
+        $.ajax({
+          type: "GET",
+          url: "/content",  
+          datatype: "json",
+          success:function(data){
+              that.updateState(data);
+              console.log(data);
+          },
+          error: function(request, status, error) {
+                console.log(error);
+              }
+        });
+      }
+
+      renderList() {
+       // this.reriveData();
+        return this.state.contents.map((content) => {
+            return (   
+            <div id="content" className="col-xl-6">
+            <h1 className="title">{content.title}</h1>
+            <div className="social-shares">
+                <div className="share-count" id="counter_shares">{content.shares}<span> Shares</span></div>
+                <div className="share-widget share-article">
+                    <div className="social">
+                        <a className="facebook" href="#">
+                            <img id="id_social" src="http://www.boldmethod.com/images/social/share-facebook-long-v2.png" onClick={this.increamentCounter(content._id)}/>
+                        </a>
+                    </div>
+                    <div className="social">
+                        <a href="#" className="twitter">
+                        <img id="id_social" src="https://images.squarespace-cdn.com/content/v1/563e2841e4b09a6ae020bd67/1530018807349-YP4LPA0C7INKN2XL0IM2/ke17ZwdGBToddI8pDm48kMJPBCPJsiaclxZdF5CWJnlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIMrHLJAYcctYfGny_wGpS1LuO2Ox8Cjc1KavDC4kMrXc/button+share+on+twitter.png" onClick={this.increamentCounter(content._id)}/>
+                        </a>
+                    </div> 
+                </div>									
+            </div>  
+           <img id="img" src={content.image}></img>
+              <div className="author" >
+                  <a href="https://www.iflscience.com/team/madison-dapcevich/" className="image">
+                    <img id="author_img"src={content.ImgAuther}/> </a>
+                        <h5 className="name">{content.Auther}</h5>
+                        <span className="date">{content.date}</span>
+                       </div>      	
+                        <div className="article-content">
+                              <p>{content.description}</p>
+                       </div>
+                  </div>  
+              )})
+           }
+         
+        render(){
+            if(this.state.contents.length){
+                return (
+                <div>{this.renderList()}</div>
+                );
+            }
+            else
+            {
+                return (
+                <div>Loading...</div>
+                );
+            }}}
 ReactDOM.render(<App />, document.getElementById("content"))
--- Project ANM Starter/Module1/server/index.js
+++ Project ANM Starter/Module1/server/index.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const db = require('../../Database/index.js')
+const db = require('../db.js')
 let app = express();
 
 app.use(bodyParser.json()); 
 app.use(express.static(__dirname + '/../client/src/dist'));
 
-// const Content = require('../.././Database/index.js').Content;
+const Content = require('../db.js').Content;
 
 
-// app.get('/content', function (req, res) {
-//   // console.log("contentcour")
-//   Content.find({}).exec((err,content) => { 
-//       if(err){
-//         console.log(err);
-//         req.send()
-//       }
-//       res.json(content)});
-// });
 
-// app.get("/save",(req,res)=>{
-//   db.save()
-//   res.send("saveed")
-// })
+app.post('/shares', function (req, res) {
+    console.log("sucees post increaments shares")
+    const id = req.body.user_id;
+    // db.Content.update(  { _id:id} , { $set: { 'shares' : shares + 1  } } );
+   Content.update(
+      {_id: id},
+      {$inc: {shares: 1}}
+    );
+  }
+)
+
+app.get('/content', function (req, res) {
+  // console.log("contentcour")
+  console.log("get datat from server");
+  Content.find({}).exec((err,content) => { 
+      if(err){
+        console.log(err);
+        req.send()
+      }
+      res.json(content)});
+});
+
+app.get("/save",(req,res)=>{
+  db.save()
+  res.send("saveed")
+})
 
 app.get("*",(req,res)=>{
   app.use(express.static(__dirname + '/../client/src/dist'));
