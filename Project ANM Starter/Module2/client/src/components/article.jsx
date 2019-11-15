b37f465e7f4d5c7b57db2503f314769097050b0e
noursaqqa <noursaqqa7@gmail.com>
2019-11-15 18:57:37 +0200

    done::

--- Project ANM Starter/Module2/client/src/components/article.jsx
+++ Project ANM Starter/Module2/client/src/components/article.jsx
@@ -13,7 +13,7 @@ import ReactDom from 'react-dom';
             <div className="postspace"> 
                 <img className="imagewrap" src={this.props.story.image} /> {/*image div*/}
                 <div className="post-content">
-                <h4 className="title">{this.props.story.title}</h4> {/*title div*/}
+                <h4 className="title">{this.props.story.title}</h4> {/*title div*/} <a href="#"></a>
                 </div>
             </div>
       )
